a7abde63895818a6958cbf09a5ca716e48cc6ad8
rustamnavoyan <rustam.navoyan@gmail.com>
2022-10-29 09:59:08 +0400

    Simplify main method

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Animal> animal = new ArrayList<Animal>();
-       Cat c1=new Cat();
-       animal.add(c1);
-       Dog d1=new Dog();
-        animal.add(d1);
-        Cat c2=new Cat();
-        animal.add(c1);
-        Dog d2=new Dog();
-        animal.add(d2);
-        Cat c3=new Cat();
-        animal.add(c1);
-        Dog d3=new Dog();
-        animal.add(d3);
-        for (int i=0; i<animal.size(); i++){
-            animal.get(i).makeNoice();
-            animal.get(i).eat();
+        List<Animal> animal = new ArrayList<>();
+        animal.add(new Cat());
+        animal.add(new Dog());
+        animal.add(new Cat());
+        animal.add(new Dog());
+        animal.add(new Cat());
+        animal.add(new Dog());
+        for (Animal value : animal) {
+            value.makeNoice();
+            value.eat();
         }
     }
 }
